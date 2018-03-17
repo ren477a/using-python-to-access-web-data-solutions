760d5c0375c4fabfc8c8331be0d813eed6a19400
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2018-03-17 19:54:24 +0800

    Create geojsonapi.py

---
+++ geojsonapi.py
@@ -0,0 +1,21 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+
+# Note that Google is increasingly requiring keys
+# for this API
+serviceurl = 'http://py4e-data.dr-chuck.net/geojson?'
+
+address = input('Enter location: ')
+
+url = serviceurl + urllib.parse.urlencode(
+    {'address': address})
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url)
+data = uh.read().decode()
+print('Retrieved', len(data), 'characters')
+try:
+    js = json.loads(data)
+except:
+    js = None
+print('Place id', js['results'][0]['place_id'])
